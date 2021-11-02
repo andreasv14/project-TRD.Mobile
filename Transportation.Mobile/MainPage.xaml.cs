7253e86ae03a79eff43f75a7c7d3b637399f1e9a
Andreas Vasiliou <andreas.vasiliou14@outlook.com>
2021-11-02 09:57:09 +0200

    Init

---
+++ Transportation.Mobile/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Refit;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Transportation.Dtos;
+using Transportation.Mobile.Services;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace Transportation.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            GetCurrentLocation();
+
+            GetTransportationRouteAsync();
+        }
+
+        private async Task GetCurrentLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
+                
+                _cancellationTokenSource = new CancellationTokenSource();
+                
+                var location = await Geolocation.GetLocationAsync(request, _cancellationTokenSource.Token);
+                if (location != null)
+                {
+                    map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromMeters(100)));
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+
+        private async Task GetTransportationRouteAsync()
+        {
+            try
+            {
+                var routeDataService = RestService.For<IRouteDataService>("http://10.0.2.2:5001");
+
+                var route = await routeDataService.GetTransportationRoute(1);
+                foreach (var busStop in route.PinPointLocations)
+                {
+                    map.Pins.Add(new Pin() { Position = new Position(busStop.Latitude, busStop.Longitude) });
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+          
+        }
+    }
+}
---
+++ Transportation.Mobile/Services/IRouteDataService.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using System.Threading.Tasks;
+using Transportation.Dtos;
+
+namespace Transportation.Mobile.Services
+{
+    public interface IRouteDataService
+    {
+        [Get("/routes/transportation-route")]
+        Task<TransportationRouteDto> GetTransportationRoute(int transportationId);
+    }
+}
